0f155ba75e6bcffd8ef16dd364984a5ca704339a
Denis Alevi <mail@denisalevi.de>
2019-11-25 22:06:17 +0100

    Last version before OS reinstall

--- texfig.py
+++ texfig.py
@@ -10,10 +10,11 @@ mpl.use('pgf')
 from math import sqrt
 default_width = 5.78853 # in inches
 default_ratio = (sqrt(5.0) - 1.0) / 2.0 # golden mean
+default_types = ['pdf', 'pgf']
 
 mpl.rcParams.update({
     "text.usetex": True,
-    "pgf.texsystem": "xelatex",
+    "pgf.texsystem": "pdflatex",
     "pgf.rcfonts": False,
     "font.family": "serif",
     "font.serif": [],
@@ -24,35 +25,42 @@ mpl.rcParams.update({
         # put LaTeX preamble declarations here
         r"\usepackage[utf8x]{inputenc}",
         r"\usepackage[T1]{fontenc}",
-        # macros defined here will be available in plots, e.g.:
-        r"\newcommand{\vect}[1]{#1}",
-        # You can use dummy implementations, since you LaTeX document
-        # will render these properly, anyway.
+        r"\usepackage{amsmath}",
+        r"\usepackage{bm}",
+        r"\usepackage{relsize}",
+        r"\usepackage{calc}",
     ],
+    "xtick.labelsize": "small",
 })
 
+import seaborn as sns
+# font size 11pt
+sns.set_context('paper', font_scale=10/12/0.8)
+#sns.set_context('notebook', font_scale=11/12)
 import matplotlib.pyplot as plt
 
 
 """
 Returns a figure with an appropriate size and tight layout.
 """
-def figure(width=default_width, ratio=default_ratio, pad=0, *args, **kwargs):
+def figure(width=default_width, ratio=default_ratio, tight_layout=False, pad=0, *args, **kwargs):
     fig = plt.figure(figsize=(width, width * ratio), *args, **kwargs)
-    fig.set_tight_layout({
-        'pad': pad
-    })
+    if tight_layout:
+        fig.set_tight_layout({
+            'pad': pad
+        })
     return fig
 
 
 """
 Returns subplots with an appropriate figure size and tight layout.
 """
-def subplots(width=default_width, ratio=default_ratio, *args, **kwargs):
+def subplots(*args, width=default_width, ratio=default_ratio, tight_layout=False, **kwargs):
     fig, axes = plt.subplots(figsize=(width, width * ratio), *args, **kwargs)
-    fig.set_tight_layout({
-        'pad': 0
-    })
+    if tight_layout:
+        fig.set_tight_layout({
+            'pad': 0
+        })
     return fig, axes
 
 
@@ -60,5 +68,5 @@ def subplots(width=default_width, ratio=default_ratio, *args, **kwargs):
 Save both a PDF and a PGF file with the given filename.
 """
 def savefig(filename, *args, **kwargs):
-    plt.savefig(filename + '.pdf', *args, **kwargs)
-    plt.savefig(filename + '.pgf', *args, **kwargs)
+    for t in default_types:
+        plt.savefig(filename + f'.{t}', *args, **kwargs)
